1f2b9811f9a982859c0ed1679c5c34e34dd5333f
Siddhartha Khanal <sidkhanal77@gmail.com>
2025-01-24 11:02:02 +0545

    added news model and templates

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from wagtail.models import Page
-from wagtail.models import Page
 from wagtail.fields import RichTextField
 from wagtail.admin.panels import FieldPanel
 from wagtail.images.models import Image
@@ -13,22 +12,17 @@ from wagtail.images.models import Image
 class BlogPage(Page):
 
     def get_context(self, request, *args, **kwargs):
-        """Adding custom stuff to our context."""
         context = super().get_context(request, *args, **kwargs)
-        # Get all live and public blog posts
         blogs = BlogDetailPage.objects.live().public().order_by("-first_published_at")
-        # Paginate the blogs queryset
-        paginator = Paginator(blogs, 6)  # Show 10 blogs per page
-        page = request.GET.get("page")  # Get the current page number from the request
+        paginator = Paginator(blogs, 8)
+        page = request.GET.get("page")
         try:
             blogs_paginated = paginator.page(page)
         except PageNotAnInteger:
-            # If page is not an integer, deliver the first page
             blogs_paginated = paginator.page(1)
         except EmptyPage:
-            # If page is out of range (e.g., 9999), deliver the last page
             blogs_paginated = paginator.page(paginator.num_pages)
-        # Add the paginated blogs to the context
+
         context["blogs"] = blogs_paginated
         return context
 
@@ -65,7 +59,6 @@ class BlogDetailPage(Page):
     content_panels = Page.content_panels + [
         FieldPanel("blog_title"),
         FieldPanel("blog_body"),
-        # FieldPanel("blog_publish_date"),
         FieldPanel("blog_description"),
         FieldPanel("blog_cover_image"),
     ]
--- news/models.py
+++ news/models.py
@@ -1,5 +1,41 @@
 from django.db import models
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
 from wagtail.models import Page
+from wagtail.fields import RichTextField
+from wagtail.admin.panels import FieldPanel
 
 
-class NewsPage(Page): ...
+class NewsPage(Page):
+
+    def get_context(self, request, *args, **kwargs):
+        context = super().get_context(request, *args, **kwargs)
+        news = NewsDetailPage.objects.live().public().order_by("-first_published_at")
+        paginator = Paginator(news, 6)
+        page = request.GET.get("page")
+        try:
+            news_paginated = paginator.page(page)
+        except PageNotAnInteger:
+            news_paginated = paginator.page(1)
+        except EmptyPage:
+            news_paginated = paginator.page(paginator.num_pages)
+
+        context["news"] = news_paginated
+        return context
+
+
+class NewsDetailPage(Page):
+    news_publish_date = models.DateField(auto_created=True, auto_now=True)
+    news_title = models.CharField(
+        verbose_name="News Title", max_length=255, null=True, blank=False
+    )
+    news_description = models.CharField(
+        verbose_name="News Description", max_length=255, blank=False, null=True
+    )
+    news_body = RichTextField()
+
+    content_panels = Page.content_panels + [
+        FieldPanel("news_title"),
+        FieldPanel("news_description"),
+        FieldPanel("news_body"),
+    ]
